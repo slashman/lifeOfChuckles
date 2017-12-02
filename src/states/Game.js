6834099c08aaff49d88b06a955e4039382fe4027
slash <slash@slashware.net>
2017-12-02 09:41:58 -0500

    Answer the questions, chuckles.

--- src/states/Game.js
+++ src/states/Game.js
@@ -8,15 +8,25 @@ export default class extends Phaser.State {
   init () {}
   preload () {}
 
+  createText (x, y, initialText){
+  	const text = this.add.text(x, y, initialText)
+    text.font = 'Bangers'
+    text.padding.set(10, 16)
+    text.fontSize = 40
+    text.fill = '#77BFA3'
+    text.smoothed = false
+    text.anchor.setTo(0.5)
+    return text
+  }
+
   create () {
-    const bannerText = 'Life of Chuckles'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
-    banner.font = 'Bangers'
-    banner.padding.set(10, 16)
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.smoothed = false
-    banner.anchor.setTo(0.5)
+    const banner = this.createText(this.world.centerX, 40, 'Life of Chuckles')
+	this.problemText = this.createText(this.world.centerX, 80, "")
+  	this.problemOptionA = this.createText(this.world.centerX - 100, this.game.height - 80, "")
+  	this.problemOptionB = this.createText(this.world.centerX + 100, this.game.height - 80, "")
+  	this.problemTimeText = this.createText(this.world.centerX, 120, "")
+  	this.scoreText = this.createText(this.world.centerX - 100, this.game.height - 40, '')
+  	this.hpText = this.createText(this.world.centerX + 100, this.game.height - 40, '')
 
     this.cursors = this.game.input.keyboard.createCursorKeys()
     this.zKey = this.game.input.keyboard.addKey(Phaser.KeyCode.Z)
@@ -36,6 +46,12 @@ export default class extends Phaser.State {
     
     this.game.physics.startSystem(Phaser.Physics.ARCADE)
     this.game.physics.arcade.gravity.y = 400
+    this.currentProblemIndex = 0;
+    setTimeout(()=> this.spawnProblem(), 3000)
+    this.hp = 30
+    this.score = 0
+    this.hpText.text = `HP: ${this.hp}`
+    this.scoreText.text = `Score: ${this.score}`
   }
 
   spawnBall(){
@@ -51,10 +67,66 @@ export default class extends Phaser.State {
     setTimeout(()=> this.spawnBall(), 3000)
   }
 
+  spawnProblem(){
+  	if (this.currentProblemIndex === PROBLEMS.length){
+  		this.problemTimeText.text = "You win!"
+  		return;
+  	}
+  	const problem = PROBLEMS[this.currentProblemIndex++]
+  	this.currentProblem = problem;
+  	this.problemText.text = problem.text
+  	this.problemOptionA.text = problem.answers[0]
+  	this.problemOptionB.text = problem.answers[1]
+  	this.problemTime = problem.time
+  	this.countProblemTime()
+  }
+
+  countProblemTime() {
+  	if (this.problemTime === 0){
+  		this.resolveProblem();
+  		return;
+  	}
+  	this.problemTimeText.text = this.problemTime
+  	this.problemTime--;
+  	setTimeout(()=> this.countProblemTime(), 1000)
+  }
+
+  resolveProblem() {
+  	const selectedAnswer = this.chuckles.x < this.world.centerX ? 0 : 1;
+  	if (selectedAnswer === this.currentProblem.correct){
+  		// Score, relax, go forward
+  		this.score++;
+  		this.scoreText.text = `Score: ${this.score}`
+  		this.problemTimeText.text = "Correct!"
+  	} else {
+  		this.problemTimeText.text = "Wrong!"
+  		this.damage(10)
+  		if (this.dead)
+  			return;
+  	}
+  	setTimeout(()=> this.clearProblem(), 2000)
+  	setTimeout(()=> this.spawnProblem(), 5000) // TODO: Problems come quicker everytime
+  }
+
+  clearProblem() {
+  	this.problemText.text = ""
+  	this.problemOptionA.text = ""
+  	this.problemOptionB.text = ""
+  	this.problemTimeText.text = ""
+  }
+
   getBallAt(x,y) {
   	return this.balls.find(b => {
   		return dist(x,y,b.x,b.y) < 20
   	})
+  }
+
+  damage(damage){
+  	this.hp -= damage
+	this.hpText.text = `HP: ${this.hp}`
+  	if (this.hp <= 0){
+  		this.dead = true;
+  	}
   }
 
   update () {
@@ -70,9 +142,25 @@ export default class extends Phaser.State {
 	}
   }
 
+
+
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.chuckles, 32, 32)
     }
   }
 }
+
+const PROBLEMS = [
+	{
+		text: "2 + 2",
+		answers: [4, 1],
+		correct: 0,
+		time: 10
+	},
+	{
+		text: "7 * 6",
+		answers: [42, 46],
+		correct: 0,
+		time: 10
+	},
+]
